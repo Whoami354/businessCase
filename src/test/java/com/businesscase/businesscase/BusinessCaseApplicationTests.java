20f2d509fe18af841cb9ae48e273a8f751d69f7d
WhoAmI <fabian.gregory98@hotmail.de>
2026-02-24 10:05:57 +0100

    Alles vorbereitet

---
+++ src/test/java/com/businesscase/businesscase/BusinessCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businesscase.businesscase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
