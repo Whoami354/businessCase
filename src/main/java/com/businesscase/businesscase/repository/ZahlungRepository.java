f7b4b08b17ce6136b95e09312998457db227e31d
WhoAmI <fabian.gregory98@hotmail.de>
2026-02-24 16:29:21 +0100

    Interface "ZahlungRepository" fertiggestellt

--- src/main/java/com/businesscase/businesscase/repository/ZahlungRepository.java
+++ src/main/java/com/businesscase/businesscase/repository/ZahlungRepository.java
@@ -1,4 +1,8 @@
 package com.businesscase.businesscase.repository;
 
-public interface ZahlungRepository {
+import com.businesscase.businesscase.model.Zahlung;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ZahlungRepository  extends JpaRepository<Zahlung, Long> {
+
 }
