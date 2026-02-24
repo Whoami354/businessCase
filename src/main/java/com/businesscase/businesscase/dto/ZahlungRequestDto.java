95e2aee3db2b597af84a91cb36d0ea815e34149b
WhoAmI <fabian.gregory98@hotmail.de>
2026-02-24 16:08:47 +0100

    Klasse ZahlungRequestDto fertiggestellt

--- src/main/java/com/businesscase/businesscase/dto/ZahlungRequestDto.java
+++ src/main/java/com/businesscase/businesscase/dto/ZahlungRequestDto.java
@@ -1,7 +1,23 @@
 package com.businesscase.businesscase.dto;
 
-import com.businesscase.businesscase.model.Zahlung;
+import com.businesscase.businesscase.model.Status;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
+import java.math.BigDecimal;
+
+@Getter
+@Setter
 public class ZahlungRequestDto {
 
+    @NotNull
+    private Status status;
+
+    @NotNull
+    @Pattern(regexp = "\\d{2}-\\d{2}-\\d{4}")
+    private String zahlungsdatum;
+
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
+    private BigDecimal betrag;
 }
