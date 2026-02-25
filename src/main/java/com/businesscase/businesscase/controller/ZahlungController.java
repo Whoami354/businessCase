70c17dc3b9beb44ef1645372091202daaa719826
WhoAmI <fabian.gregory98@hotmail.de>
2026-02-25 15:40:00 +0100

    PostMapping fuer Zahlungerstellen erstellt

--- src/main/java/com/businesscase/businesscase/controller/ZahlungController.java
+++ src/main/java/com/businesscase/businesscase/controller/ZahlungController.java
@@ -1,4 +1,22 @@
 package com.businesscase.businesscase.controller;
 
+import com.businesscase.businesscase.dto.ZahlungRequestDto;
+import com.businesscase.businesscase.dto.ZahlungResponseDto;
+import com.businesscase.businesscase.service.ZahlungService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("zahlungen")
+@RequiredArgsConstructor
 public class ZahlungController {
+
+    private final ZahlungService service;
+
+    @PostMapping("zahlungenErstellen")
+    public ResponseEntity<ZahlungResponseDto> create(@Valid @RequestBody ZahlungRequestDto dto) {
+        return ResponseEntity.ok(service.create(dto));
+    }
 }
--- src/main/java/com/businesscase/businesscase/service/ZahlungService.java
+++ src/main/java/com/businesscase/businesscase/service/ZahlungService.java
@@ -1,4 +1,41 @@
 package com.businesscase.businesscase.service;
 
+import com.businesscase.businesscase.dto.ZahlungRequestDto;
+import com.businesscase.businesscase.dto.ZahlungResponseDto;
+import com.businesscase.businesscase.model.Zahlung;
+import com.businesscase.businesscase.repository.ZahlungRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@RequiredArgsConstructor
 public class ZahlungService {
+
+    private final ZahlungRepository repository;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    public ZahlungResponseDto create(ZahlungRequestDto dto) {
+        Zahlung zahlung = mapToEntity(dto);
+        return mapToResponse(repository.save(zahlung));
+    }
+
+    private Zahlung mapToEntity(ZahlungRequestDto dto) {
+        return Zahlung.builder()
+                .status(dto.getStatus())
+                .zahlungsDatum(LocalDate.parse(dto.getZahlungsdatum(), formatter))
+                .betrag(dto.getBetrag())
+                .build();
+    }
+
+    private ZahlungResponseDto mapToResponse(Zahlung zahlung) {
+        return ZahlungResponseDto.builder()
+                .id(zahlung.getId())
+                .status(zahlung.getStatus())
+                .zahlungsdatum(zahlung.getZahlungsDatum().format(formatter))
+                .betrag(zahlung.getBetrag())
+                .build();
+    }
 }
