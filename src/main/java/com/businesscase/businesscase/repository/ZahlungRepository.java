f0e0d58ae928eec8bc62d03c2c05b5bdf16449aa
WhoAmI <fabian.gregory98@hotmail.de>
2026-02-26 21:09:42 +0100

    Refactoring

--- src/main/java/com/businesscase/businesscase/repository/ZahlungRepository.java
+++ src/main/java/com/businesscase/businesscase/repository/ZahlungRepository.java
@@ -4,7 +4,6 @@ import com.businesscase.businesscase.model.Zahlung;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.Set;
 
 public interface ZahlungRepository  extends JpaRepository<Zahlung, Long> {
     Optional<Zahlung> findById(Long id);
